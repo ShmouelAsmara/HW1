5b5c6eb64919c2a392dc482bc67dc8684424c93a
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-05 20:05:45 +0200

    check the random

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,49 @@
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
+
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		// I dont know how to put the range from the user so
+		// I creat an integer that decide the range
+		int range = 100;
+		int A = (int) (Math.random() * range) + 1;
+		int B = (int) (Math.random() * range) + 1;
+		int C = (int) (Math.random() * range) + 1;
+
+		while (A < B && A < C) {
+			System.out.println(A);
+			if (B < C) {
+				System.out.println(B);
+				System.out.println(C);
+			} else
+				System.out.println(C);
+			System.out.println(B);
+			return;
+
+		}
+
+		while (B < A && B < C) {
+			System.out.println(B);
+			if (A < C) {
+				System.out.println(A);
+				System.out.println(C);
+			} else
+				System.out.println(C);
+			System.out.println(A);
+			return;
+		}
+
+		while (C < A && C < B) {
+			System.out.println(C);
+			if (A < B) {
+				System.out.println(A);
+				System.out.println(B);
+			} else
+				System.out.println(B);
+			System.out.println(A);
+			return;
+		}
 	}
+
 }
