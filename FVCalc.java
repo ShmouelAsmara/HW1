0cf34d16c2883455287bff2fd683694d8c1847b8
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-06 21:16:29 +0200

    fvcalc

--- FVCalc.java
+++ FVCalc.java
@@ -3,11 +3,13 @@ public class FVCalc {
 	public static void main(String[] args) {
 		// Replace this comment with your code
 		int currentValue = Integer.parseInt(args[0]);
-		int rate = Integer.parseInt(args[1]);
+		double rate = Double.parseDouble(args[1]);
 		int years = Integer.parseInt(args[2]);
+		double rateClac = (rate / 100);
+		double futureValue = currentValue * Math.pow((1 + rateClac), years);
 
-		int futureValue = currentValue * (1 + rate) ^ years;
 		System.out.print(
-				"After" + years + "years, " + "$" + currentValue + " saved at" + rate + "% will yield $" + futureValue);
+				"After " + years + " years, a " + "$" + currentValue + " saved at " + rate + "% will yield $"
+						+ ((int) futureValue));
 	}
 }
