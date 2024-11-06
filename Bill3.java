6f933514794e6fbfc089ff8ff3799477ca2408c1
Shmouel.Asmara <shmouel.asmara@post.runi.ac.il>
2024-11-06 21:50:37 +0200

    bill3fix2

--- Bill3.java
+++ Bill3.java
@@ -9,7 +9,7 @@ public class Bill3 {
 		int bill = Integer.parseInt(args[3]);
 
 		double SplitBill = Math.ceil(bill / 3.0);
-		System.out.println("Dear " + name3 + "," + name2 + ", and " + name1 + ": pay " + SplitBill + " Shekels each.");
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + SplitBill + " Shekels each.");
 
 	}
 }
